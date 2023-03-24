592c9aa250e4fb2424b53ee279c5005e897934e0
Harsha9705 <harsdharsha@gmail.com>
2023-03-24 17:54:28 +0530

    Life

---
+++ life.js
@@ -0,0 +1,48 @@
+$('#title').click(function () {
+    $('#title').animate({fontSize:'50px'})
+    $('#title').mouseout(function () {
+        $('#title').animate({fontSize:'45px'})
+    })
+})
+
+$('#para1').click(function () {
+    $('#para1').animate({fontSize:'25px'})
+    $('##para1').mouseout(function () {
+        $('#para1').animate({fontSize:'18px'})
+    })
+})
+
+$('#d-card1').click(function () {
+    $('#d-card1').animate({width:'310px',height:'130px'})
+    $('#d-card1').mouseout(function () {
+        $('#d-card1').animate({width:'300px',height:'120px'})
+    })
+})
+
+$('#d-card2').click(function () {
+    $('#d-card2').animate({width:'310px',height:'130px'})
+    $('#d-card2').mouseout(function () {
+        $('#d-card2').animate({width:'300px',height:'120px'})
+    })
+})
+
+$('#d-card3').click(function () {
+    $('#d-card3').animate({width:'310px',height:'130px'})
+    $('#d-card3').mouseout(function () {
+        $('#d-card3').animate({width:'300px',height:'120px'})
+    })
+})
+
+$('#d-card4').click(function () {
+    $('#d-card4').animate({width:'310px',height:'130px'})
+    $('#d-card4').mouseout(function () {
+        $('#d-card4').animate({width:'300px',height:'120px'})
+    })
+})
+
+$('#d-card5').click(function () {
+    $('#d-card5').animate({width:'310px',height:'130px'})
+    $('#d-card5').mouseout(function () {
+        $('#d-card5').animate({width:'300px',height:'120px'})
+    })
+})
